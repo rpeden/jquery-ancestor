bf8acd8fdb5cd988181c5600470343ba53cb0b36
Ryan Peden <ryan04@gmail.com>
2014-12-18 13:57:24 -0500

    addded more grandparent functions

--- jquery.ancestor.js
+++ jquery.ancestor.js
@@ -21,6 +21,30 @@
     		console.log("You should be ashamed of yourself.");
     	}
 
-    	return this.parent().parent().parent();   
+    	return this.parent().parent().parent().parent();   
+    };
+
+    $.fn.greatGreatGreatGrandparent = function() {
+    	if(console && console.log){
+    		console.log("People like you drive other developers to drink.");
+    	}
+
+    	return this.parent().parent().parent().parent().parent();   
+    };
+
+    $.fn.greatGreatGreatGreatGrandparent = function() {
+    	if(console && console.log){
+    		console.log("A cat stomping on a keyboard could probably write better code than you.");
+    	}
+
+    	return this.parent().parent().parent().parent().parent().parent(); 
+    };
+
+    $.fn.greatGreatGreatGreatGreatGrandparent = function() {
+    	if(console && console.log){
+    		console.log("Are you a former COBOL programmer?");
+    	}
+
+    	return this.parent().parent().parent().parent().parent().parent().parent(); 
     };
 }( jQuery ));
